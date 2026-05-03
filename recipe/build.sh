64935e7ace14c6774c4e81c62d3f0e99916ed4ca
Benjamin Bertrand <benjamin.bertrand@maxiv.lu.se>
2026-05-03 15:26:22 +0200

    Add tango-admin-dbg package on Linux
    - Use staging output to add dbg package
    - Move to ninja

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,19 @@
-mkdir build
-cd build
 cmake ${CMAKE_ARGS} \
-      -DCMAKE_BUILD_TYPE=Release \
+      -G Ninja \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_VERBOSE_MAKEFILE=true \
       -DBUILD_TESTING=OFF \
-      ..
+      -S . \
+      -B build
 
-make -j $CPU_COUNT
-make install
+cmake --build build
+cmake --install build
+
+# Separate debugging symbols on Linux
+if [ -n "${OBJCOPY}" ]; then
+  mkdir -p ${PREFIX}/lib/debug
+  ${OBJCOPY} --only-keep-debug ${PREFIX}/bin/tango_admin ${PREFIX}/lib/debug/tango_admin.dbg
+  chmod 664 ${PREFIX}/lib/debug/tango_admin.dbg
+  ${OBJCOPY} --strip-debug ${PREFIX}/bin/tango_admin
+  ${OBJCOPY} --add-gnu-debuglink=${PREFIX}/lib/debug/tango_admin.dbg ${PREFIX}/bin/tango_admin
+fi
